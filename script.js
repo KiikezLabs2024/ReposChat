2b52c629633f802a0bb28453e93ad14b696c84ad
KiikezLabs2024 <it.tecnico2024@gmail.com>
2024-10-28 15:38:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function sendMessage() {
-    const messageInput = document.getElementById('messageInput');
-    const messageText = messageInput.value.trim();
-    if (messageText) {
-        const messagesContainer = document.getElementById('messages');
-        const messageElement = document.createElement('div');
-        messageElement.textContent = messageText;
-        messagesContainer.appendChild(messageElement);
-        messageInput.value = '';
-        messagesContainer.scrollTop = messagesContainer.scrollHeight; // Desplazarse hacia abajo
-    }
-}
 
